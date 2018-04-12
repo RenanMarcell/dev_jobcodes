c44882c7d0f4774bc4fe3ff1e18049d9266fd192
renan <renanm@outlook.com>
2018-04-12 00:11:52 -0300

    rename login to index

--- js/retrieve_data.js
+++ js/retrieve_data.js
@@ -29,7 +29,7 @@ var remaining_questions = [];
     setTimeout( () => {
         alert("Tempo esgotado");
         auth().signOut();
-        window.location.replace('https://renanmarcell.github.io/dev_jobcodes/login.html');
+        window.location.replace('https://renanmarcell.github.io/dev_jobcodes/index.html');
     }, 600000);
     setTimeout( () => {
         alert("Resta 1 minuto");
